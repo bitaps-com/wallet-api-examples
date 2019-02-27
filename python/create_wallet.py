b910ef9db49c9013151187c17b219c7c5f3730d0
Aleksey Karpov <admin@bitaps.com>
2019-02-27 14:26:10 +0400

    Update create_wallet.py

--- python/create_wallet.py
+++ python/create_wallet.py
@@ -1,1 +1,9 @@
+import requests
+import json
 
+url = "https://api.bitaps.com/btc/v1/create/wallet"
+options = {}
+response = requests.request("POST", url, data=json.dumps(options))
+result = json.loads(response.text)
+print("Wallet without any aditional options:")
+print(result)
