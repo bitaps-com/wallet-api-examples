2bcf7a8e54c9d5cc9847aae724f591b3f7e3aa9e
4tochka <admin@bitaps.com>
2019-03-12 14:11:53 +0400

    php create wallet address

--- php/README.md
+++ php/README.md
@@ -1,8 +1,8 @@
-##PHP Examples for wallet API
+## PHP Examples for wallet API
 
 ### Step 1 Create wallet:
 
-To create wallet use this API endoints:
+To create wallet use this API endpoints:
 
   - https://api.bitaps.com/btc/testnet/v1/create/wallet  (testnet)
   - https://api.bitaps.com/btc/v1/create/wallet
@@ -28,3 +28,40 @@ Result:
         [wallet_id_hash] => 0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a
         [currency] => BTC
     )
+
+### Step 2 Create wallet address:
+
+Wallet can have an unlimited number of addresses to replenish or accept payments. Each address can be created with
+individual callback link or use wallet default callback link if it was specified while creating wallet. In case no
+callback links specified while creating address and wallet, payment notifications will not be sent.
+
+Incoming payments will be credited to the wallet balance upon confirmation on bitcoin transaction. The number of 
+confirmations depends on the amount of payment.
+
+  - less than 0.1 BTC  1 confirmation required 
+  - 0.1 BTC - 1 BTC  2 confirmation required 
+  - more or equal 1 BTC 3 confirmation required 
+  
+  
+    $params = array("wallet_id"=> "0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a");
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet/payment/address",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => json_encode($params)));
+    $response = curl_exec($curl);
+
+Response:
+
+    stdClass Object
+    (
+        [invoice] => invNsPyYePpvQZfY9qFCSMwVd63oDuHRzrvvjsPvbiXGiWS2Z3ktA
+        [payment_code] => PMTvKzyrTu8npZ7n1g6F6aVcmKXRza39wo9Axk4shamXppm3gmMHU
+        [address] => 3AB2qrrBRcxSMNYrPAJ21WhCWcNJXpjvVx
+        [confirmations] => depends on the amount
+        [notification_link_domain] => 
+        [wallet_id_hash] => 0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a
+        [currency] => BTC
+    )
+
---
+++ php/create_wallet_address.php
@@ -0,0 +1,102 @@
+<?php
+
+echo "Example 1\n\n";
+echo "Create wallet address:\n";
+$params = array("wallet_id"=> "BTCvzHmmL1ijiBou7U8u9Ye1F4PxDtDQccYSvzAYNvJz866kCv9KA");
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet/payment/address",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
+
+
+echo "Example 2\n\n";
+echo "Create wallet address use wallet_id_hash:\n";
+$params = array("wallet_id"=> "0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a");
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet/payment/address",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
+
+echo "Example 3\n\n";
+echo "Create wallet address with callback link:\n";
+$params = array("wallet_id"=> "0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a",
+                "callback_link" => "http://testx.bitaps.com/simple/callback");
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet/payment/address",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
+
+
+echo "Example 4\n\n";
+echo "Create wallet address with exactly count of confirmation notifications:\n";
+$params = array("wallet_id"=> "0ddfcc11d0cd9490b23944b3648268981176eb8ebd04f7bbd29506bc2b8dba5a",
+                "callback_link" => "http://testx.bitaps.com/simple/callback",
+                "confirmations" => 4);
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet/payment/address",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
