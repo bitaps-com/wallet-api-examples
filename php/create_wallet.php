f7f5c2838a9b5af31ebc8a91d7aeddd1aab728d6
4tochka <admin@bitaps.com>
2019-03-12 13:19:55 +0400

    php create wallet

--- php/create_wallet.php
+++ php/create_wallet.php
@@ -1,1 +1,77 @@
+<?php
 
+echo "Example 1\n\n";
+echo "Create wallet:\n";
+$params = array();
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
+
+
+
+
+echo "Example 2\n\n";
+echo "Create wallet with callback url http://testx.bitaps.com/simple/callback:\n";
+
+$params = array("callback_link" => "http://testx.bitaps.com/simple/callback");
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
+
+
+echo "Example 3\n\n";
+echo "Create wallet with password:\n";
+
+$params = array("callback_link" => "http://testx.bitaps.com/simple/callback",
+                "password" => "secret");
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.bitaps.com/btc/v1/create/wallet",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params)
+));
+
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r(json_decode($response));
+}
